6b480ed46cd2d95adf90abd74e0d70e45eb3eed9
FahimHassanMollah <fahimhassan0197@gmail.com>
2021-04-27 23:52:17 +0600

    all idea show and normal navigation done

--- index.js
+++ index.js
@@ -7,6 +7,46 @@ const PORT = process.env.PORT || 5000;
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+const ideas = [
+    {
+        id: 1,
+        title: 'idea 1',
+        description: 'idea 1 description',
+        allowComments: true,
+        status: 'public'
+    },
+    {
+        id: 2,
+        title: 'idea 2',
+        description: 'idea 2 description',
+        allowComments: false,
+        status: 'private'
+    },
+    {
+        id: 3,
+        title: 'idea 3',
+        description: 'idea 3 description',
+        allowComments: true,
+        status: 'public'
+    },
+    {
+        id: 4,
+        title: 'idea 4',
+        description: 'idea 4 description',
+        allowComments: true,
+        status: 'public'
+    },
+
+]
+
+app.get('/ideas', function (req, res) {
+    res.render('ideas/index',{
+        ideas
+    });
+});
+app.get('/about', function (req, res) {
+    res.render('about');
+});
 app.get('/', function (req, res) {
     res.render('home');
 });
